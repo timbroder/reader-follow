14773a6dc4dce4fa6669f339fcfd1008ecf02824
Tim Broder <timothy.broder@gmail.com>
2012-02-04 16:16:33 -0500

    fixing comments for compact view

--- django-app/templates/comments.js
+++ django-app/templates/comments.js
@@ -1,5 +1,11 @@
 {% load cache %} {% autoescape on %} {% cache 86400 comments article.id %}
 var html = '{% for comment in comment_list %}<p>{{ comment.user.username }} <em>({{ comment.submit_date }})</em></p><p>{{ comment.comment }}</p>{% endfor %}';
-jQuery('.{{ sha }}').find('.entry-comments').html(html);
+var $expanded = jQuery('.{{ sha }}').find('.entry-comments');
+if ($expanded.size() > 0) {
+	$expanded.html(html);
+}
+else {
+	jQuery('.read-{{ sha }}').find('.card-comments').html(html);
+}
 {% endcache %}
 {% endautoescape %}
